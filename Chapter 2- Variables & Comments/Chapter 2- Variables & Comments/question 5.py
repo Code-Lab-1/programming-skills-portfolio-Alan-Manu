96de0d9510d4197ae66fa5c6fa3a4ffe13aaa8dc
Alan-Manu <113759295+Alan-Manu@users.noreply.github.com>
2022-10-30 00:08:47 +0400

    Add files via upload

---
+++ Chapter 2- Variables & Comments/Chapter 2- Variables & Comments/question 5.py
@@ -0,0 +1,7 @@
+print("A girl heads to a computer shop to buy some USB sticks.")
+print("She loves USB sticks and wants as many as she can get for £50. They are £6 each.")
+print("how many USB sticks can she buy and how much money will she have left?" )
+a=50//6
+print("NUMBER OF USB STICKS BOUGHT",a)
+b=50%6
+print("AMOUNT OF MONEY LEFT",b)
