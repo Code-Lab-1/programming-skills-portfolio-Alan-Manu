583e009e61a7f1f9e22c8895f88d0e27ac6ec512
Alan-Manu <113759295+Alan-Manu@users.noreply.github.com>
2022-10-30 13:27:08 +0400

    Add files via upload

---
+++ Chapter 5- Dictionaries/question 1.py
@@ -0,0 +1,6 @@
+#dictionaries
+person = {'first_name': 'rahul','last_name': 'malhotha','age': 43,'city': 'mumbai',}
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
